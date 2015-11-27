48e9bcef43983aa2e233682f1599ac38056be559
Allan Maxwell Blair <a.maxwell.blair@gmail.com>
2015-11-27 10:56:35 -0500

    implement iteration mechancis

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,7 +1,34 @@
 ##Description: Create a merge sort algorithm
 
 def merge_sort(arr)
-  (arr.length % 2) == 0 ? nil : odd = 1
-  (arr.length / 2)
+
+  half = arr.length / 2
+  half_arr = []
+  mover = 0
+  while half < 2
+    half_arr.shift(half)
+    half = half / 2
+  end
+
+
+
+  half_arr.each do |h|
+    i = 0
+    count = 0
+    while count < arr.length
+      while i < h
+        if arr[i] > arr[i+1]
+          mover = arr[i]
+          arr[i]=arr[i+1]
+          arr[i+1]=mover
+        end
+        i += 1
+      end
+      count += h
+    end
+  end
+
+
+
 
 end
