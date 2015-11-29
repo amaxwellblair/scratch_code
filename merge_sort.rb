38d5dec63807942d2862706efe0a59309992f628
Allan Maxwell Blair <a.maxwell.blair@gmail.com>
2015-11-28 23:25:21 -0500

    merge sort progress 1

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,31 +2,54 @@
 
 def merge_sort(arr)
 
-  half = arr.length / 2
-  half_arr = []
-  mover = 0
-  while half < 2
-    half_arr.shift(half)
-    half = half / 2
-  end
-
-
-
-  half_arr.each do |h|
-    i = 0
-    count = 0
-    while count < arr.length
-      while i < h
-        if arr[i] > arr[i+1]
-          mover = arr[i]
-          arr[i]=arr[i+1]
-          arr[i+1]=mover
-        end
-        i += 1
+  def sorted?(arr)
+    bin = 0
+    arr.each_with_index do |element,i|
+      if element < arr[i-1]
+        return false
       end
-      count += h
     end
+    return true
   end
+
+  i = 0
+  grouper = []
+  while i < arr.length
+    grouper << [arr[i], arr[i+1]]
+    i += 2
+  end
+
+  if arr % 2 == 1
+    modified = grouper.pop << arr[-1]
+    grouper << modified
+  end
+  i = 0
+  mover = 0
+  ## basic pairs
+  while i < grouper.length
+    j = 0
+    while j < grouper[i].length
+      if grouper[j] > grouper[j+1]
+        mover = grouper[j]
+        grouper[j] = grouper[j+1]
+        grouper[j+1] = mover
+      end
+      j += 1
+    end
+    i += 1
+  end
+
+  ## merge sort begins
+
+  i = 0
+  new_grouper = []
+  while i < grouper.length
+    j = 0
+    z = 0
+    while j < (grouper[i].length + grouper[i+1].length)
+      while z < (grouper[i].length + grouper[i+1].length)
+        if grouper[i][j] < grouper[i+1][z]
+          new_grouper << gr
 
 
 
